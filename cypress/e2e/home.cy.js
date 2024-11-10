2a08c4adc463318152c063649115277a65b162c9
rusgyn <rusgynmagsanay@gmail.com>
2024-11-10 15:24:13 -0500

    Modify cypress initializers. Create category and products when cypress starts.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,20 @@
-describe("Home Page", () => {
+describe("The Jungle Rails", () => {
   it("should load the home page", () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.title().should('include', 'Jungle');
     cy.get('.navbar-brand').should('exist')
     cy.contains('Welcome to The Jungle').should('exist');
   });
 
+  it("There is products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("have.length", 2);
+  });
+
 });
+
